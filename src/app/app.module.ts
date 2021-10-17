dac8c73ce46fc8dfba27bc9ad388e3a6481224b8
Kana Otawara <shimmer22.0204@gmail.com>
2021-10-18 00:39:56 +0900

    create modal-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ModalPageComponent } from './modal-page/modal-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ModalPageComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ src/app/modal-page/modal-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-page',
+  templateUrl: './modal-page.component.html',
+  styleUrls: ['./modal-page.component.css']
+})
+export class ModalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
