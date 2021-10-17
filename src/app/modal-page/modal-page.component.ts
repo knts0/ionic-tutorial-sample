0f6c56a26d9349d1bd205d128565cc7e4edbe3d6
Kana Otawara <shimmer22.0204@gmail.com>
2021-10-18 01:12:50 +0900

    add close button

--- src/app/modal-page/modal-page.component.ts
+++ src/app/modal-page/modal-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-page',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public mocalController: ModalController,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onClose() {
+    this.mocalController.dismiss();
   }
 
 }
